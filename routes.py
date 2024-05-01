41949517f2d55da18d56a7bdde334f112a1e7552
jack.sare <jacksare@neinnovationlabs.com>
2024-05-01 15:19:08 -0500

    pushing changes

--- routes.py
+++ routes.py
@@ -8,7 +8,32 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    tasks = []
+    with open('todo.txt', 'r') as f:
+        for line in f.readlines():
+            try:
+                split_str = line.strip('\n').split(' ')
+                datecreated = datetime.fromtimestamp(int(split_str[0])).strftime('%Y-%m-%d')
+                duedate = int(split_str[1])
+                current_time = int(time.time())
+                description = ' '.join(i for i in split_str if i.isdigit() == False)
+                isCompleted = True if split_str[len(split_str) - 1].isdigit() else False
+                isOverdue = current_time > duedate and isCompleted == False
+                isPending = current_time < duedate and isCompleted == False
+                duedate = datetime.fromtimestamp(duedate).strftime('%Y-%m-%d')
+                data = {
+                    'DateCreated': datecreated,
+                    'DueDate': duedate,
+                    'Description': description,
+                    'IsCompleted': isCompleted,
+                    'IsPending': isPending,
+                    'IsOverdue': isOverdue,
+                    'taskid': split_str[0]
+                }
+                tasks.append(data)
+            except: pass
+                
+    return render_template('home.html', data=tasks)
 
 @app.route('/add', methods=['POST'])
 def add():
@@ -25,18 +50,39 @@ def add():
 @app.route('/mark_complete/<task_id>', methods=['GET', 'POST'])
 def markComplete(task_id):
     with open('todo.txt', 'r') as f:
-        tasks = [task.strip('/n').strip() for task in f.readlines()]
+        tasks = [task.strip('\n').strip() for task in f.readlines()]
     
+    lines = []
     for line in tasks:
         parsedId = line.split(' ')[0]
         if parsedId == task_id:
             markedComplete = int(time.time())
+            line += f" {markedComplete}"
+        lines.append(line)
     
+    with open('todo.txt', 'w') as f:
+        for line in lines:
+            f.write(line + '\n')
+
     return redirect(url_for('home'))
 
 @app.route('/delete_task/<task_id>', methods=['GET', 'POST'])
 def deleteTask(task_id):
-    pass
+    lines = []
+    with open('todo.txt', 'r') as f:
+        tasks = [task.strip('\n').strip() for task in f.readlines()]
+    for line in tasks:
+        parseId = line.split(' ')[0]
+        if parseId == task_id:
+            continue
+        else:
+            lines.append(line)
+
+    with open('todo.txt', 'w') as f:
+        for line in lines:
+            f.write(line + '\n')
+    
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port='8765')
